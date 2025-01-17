1529fb66545fe3ec93ee53971f5cc6a4399298fa
Santiago <santiagolozadafp23@ibf.cat>
2025-01-17 20:08:38 +0100

    views

---
+++ resources/js/views/admin/authors/index.vue
@@ -0,0 +1,20 @@
+<!-- Codigo HTML -->
+<template>
+    <h1>Hola</h1>
+    <h2>{{ test }}</h2>
+    <input type="text" name="" id="" v-model="test" />
+</template>
+
+<!-- Codigo JS -->
+<script>
+import { ref } from "vue";
+
+const test = ref("Hola Mundo");
+
+onMounted(() => {
+    console.log("Hola");
+});
+</script>
+
+<!-- Codigo CSS (Scoped solo afectara a los componentes de este archivo) -->
+<style scoped></style>
