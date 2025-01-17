789722947782d4721680c7d105b9ac492defa2d3
Santiago <santiagolozadafp23@ibf.cat>
2025-01-17 19:10:20 +0100

    api2

--- app/Http/Controllers/Api/AuthorController.php
+++ app/Http/Controllers/Api/AuthorController.php
@@ -7,11 +7,39 @@ use Illuminate\Http\Request;
 
 use App\Models\Author;
 
+use Illuminate\Support\Facades\Validator;
+
 class AuthorController extends Controller
 {
     public function index()
     {
         $authors = Author::all();
         return response()->json(['status' => 485, 'success' => true, 'data' => $authors]);
+    }
+
+    public function store(Request $request)
+    {
+        // Primer metodo
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'max:255'],
+            'surname' => ['required', 'max:255'],
+            'email' => ['required', 'unique:Author']
+        ]);
+
+        $data = $validator->validated();
+
+        $author = Author::create($data);
+
+        return response()->json(['status' => 485, 'success' => true, 'data' => $author]);
+
+
+        // Segundo metodo
+        // $author = new Author();
+        // $author->name = $request->name;
+        // $author->surname = $request->surname;
+        // $author->email = $request->email;
+        // $author->save();
+
+        return $request->all();
     }
 }
