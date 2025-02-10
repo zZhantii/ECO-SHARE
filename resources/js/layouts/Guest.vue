000b331f1d32e94a19ba040cc18c9861c2499b28
Santiago <santiagolozadafp23@ibf.cat>
2025-02-10 20:17:30 +0100

    register and styles

--- resources/js/layouts/Guest.vue
+++ resources/js/layouts/Guest.vue
@@ -1,13 +1,41 @@
 <template>
-    <div>
-        <header><Navbar /></header>
+    <div class="container-register">
+        <header v-if="!hideHeader"><Navbar /></header>
 
         <router-view></router-view>
 
-        <AppFooter></AppFooter>
+        <AppFooter v-if="!hideHeader"></AppFooter>
     </div>
 </template>
 <script setup>
 import Navbar from "./Nav.vue";
 import AppFooter from "./AppFooter.vue";
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+// Variables para ocultar el header y el footer
+const hideHeader = ref(false);
+const hideFooter = ref(false);
+
+onMounted(() => {
+    // Coger la ruta actual
+    const [firstPart, routeName] = route.name.split(".");
+    // console.log(routeName);
+
+    // Si la ruta es register, oculta el header y el footer
+    if (routeName === "register") {
+        hideHeader.value = true;
+        hideFooter.value = true;
+    }
+});
 </script>
+
+<style scoped>
+.container-register {
+    display: flex;
+    align-items: center;
+    min-height: 100vh;
+}
+</style>
--- resources/js/views/register/index.vue
+++ resources/js/views/register/index.vue
@@ -1,54 +1,94 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center my-5">
+    <main class="container">
+        <section class="row justify-content-center my-5">
             <div class="col-md-6">
-                <div class="card border-0 shadow-sm">
-                    <div class="card-header bg-transparent">{{ $t('register') }}</div>
+                <div class="card border-0 bg-transparent">
+                    <div
+                        class="card-header bg-transparent d-flex justify-content-around align-items-center"
+                    >
+                        <img
+                            src="/images/Logo_es.svg"
+                            alt="Logotipo"
+                            height="150px"
+                        />
+                        <h1>{{ $t("¡Registrate ya!") }}</h1>
+                    </div>
                     <div class="card-body">
                         <form @submit.prevent="submitRegister">
                             <div class="">
-                                <!-- Email -->
+                                <!-- Name -->
                                 <div class="mb-3">
-                                    <label for="name" class="form-label">{{ $t('name') }}</label>
-                                    <input v-model="registerForm.name" id="name" type="text" class="form-control" autofocus>
+                                    <input
+                                        v-model="registerForm.name"
+                                        id="name"
+                                        type="text"
+                                        :placeholder="$t('name')"
+                                        class="form-control"
+                                        autofocus
+                                    />
                                     <!-- Validation Errors -->
                                     <div class="text-danger mt-1">
-                                        <div v-for="message in validationErrors?.name">
+                                        <div
+                                            v-for="message in validationErrors?.name"
+                                        >
                                             {{ message }}
                                         </div>
                                     </div>
                                 </div>
+                                <!-- Email -->
                                 <div class="mb-3">
-                                    <label for="email" class="form-label">{{ $t('email') }}</label>
-                                    <input v-model="registerForm.email" id="email" type="email" class="form-control" autocomplete="username">
+                                    <input
+                                        v-model="registerForm.email"
+                                        id="email"
+                                        type="email"
+                                        :placeholder="$t('email')"
+                                        class="form-control"
+                                        autocomplete="username"
+                                    />
                                     <!-- Validation Errors -->
                                     <div class="text-danger mt-1">
-                                        <div v-for="message in validationErrors?.email">
+                                        <div
+                                            v-for="message in validationErrors?.email"
+                                        >
                                             {{ message }}
                                         </div>
                                     </div>
                                 </div>
                                 <!-- Password -->
                                 <div class="mb-4">
-                                    <label for="password" class="form-label">
-                                        {{ $t('password') }}
-                                    </label>
-                                    <input v-model="registerForm.password" id="password" type="password" class="form-control" autocomplete="current-password">
+                                    <input
+                                        v-model="registerForm.password"
+                                        id="password"
+                                        type="password"
+                                        :placeholder="$t('password')"
+                                        class="form-control"
+                                        autocomplete="current-password"
+                                    />
                                     <!-- Validation Errors -->
                                     <div class="text-danger-600 mt-1">
-                                        <div v-for="message in validationErrors?.password">
+                                        <div
+                                            v-for="message in validationErrors?.password"
+                                        >
                                             {{ message }}
                                         </div>
                                     </div>
                                 </div>
                                 <div class="mb-4">
-                                    <label for="password_confirmation" class="form-label">
-                                        {{ $t('confirm_password') }}
-                                    </label>
-                                    <input v-model="registerForm.password_confirmation" id="password_confirmation" type="password" class="form-control" autocomplete="current-password">
+                                    <input
+                                        v-model="
+                                            registerForm.password_confirmation
+                                        "
+                                        id="password_confirmation"
+                                        type="password"
+                                        :placeholder="$t('confirm_password')"
+                                        class="form-control"
+                                        autocomplete="current-password"
+                                    />
                                     <!-- Validation Errors -->
                                     <div class="text-danger-600 mt-1">
-                                        <div v-for="message in validationErrors?.password_confirmation">
+                                        <div
+                                            v-for="message in validationErrors?.password_confirmation"
+                                        >
                                             {{ message }}
                                         </div>
                                     </div>
@@ -56,8 +96,12 @@
 
                                 <!-- Buttons -->
                                 <div class="flex items-center justify-end mt-4">
-                                    <button class="btn btn-primary" :class="{ 'opacity-25': processing }" :disabled="processing">
-                                        {{ $t('register') }}
+                                    <button
+                                        class="btn btn-primary w-100"
+                                        :class="{ 'opacity-25': processing }"
+                                        :disabled="processing"
+                                    >
+                                        {{ $t("register") }}
                                     </button>
                                 </div>
                             </div>
@@ -65,14 +109,13 @@
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </section>
+    </main>
 </template>
 
 <script setup>
+import useAuth from "@/composables/auth";
 
-import useAuth from '@/composables/auth'
-
-const { registerForm, validationErrors, processing, submitRegister } = useAuth();
-
+const { registerForm, validationErrors, processing, submitRegister } =
+    useAuth();
 </script>
