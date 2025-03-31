3a018e0fcda800dea95c7b59e93c813e794401ab
mario4off <mario31991@gmail.com>
2025-03-31 17:25:03 +0200

    buscador tripFinder capado par fechas anteriores a hoy

--- resources/js/components/TripFinder.vue
+++ resources/js/components/TripFinder.vue
@@ -1,27 +1,64 @@
 <template>
-    <form @submit.prevent="submitFinder" id="trip-finder" class="d-block d-md-flex justify-content-between">
+    <form
+        @submit.prevent="submitFinder"
+        id="trip-finder"
+        class="d-block d-md-flex justify-content-between"
+    >
         <div
-            class="col-none-12 col-md-9 d-block d-md-flex flex-row justify-content-around align-items-center gap-4 p-none-4 p-1 m-1">
+            class="col-none-12 col-md-9 d-block d-md-flex flex-row justify-content-around align-items-center gap-4 p-none-4 p-1 m-1"
+        >
             <IconField class="col-none-11 col-md-3 ms-none-0 m-1 p-0">
                 <InputIcon class="ms-1 pi pi-circle" />
-                <InputText type="text" id="origin" placeholder="De" class="rm-border w-100" />
+                <InputText
+                    type="text"
+                    id="origin"
+                    placeholder="De"
+                    class="rm-border w-100"
+                />
             </IconField>
             <IconField class="col-none-11 col-md-3 p-0 m-1">
                 <InputIcon class="ms-1 pi pi-circle" />
-                <InputText type="text" placeholder="A" id="destination" class="rm-border w-100" />
+                <InputText
+                    type="text"
+                    placeholder="A"
+                    id="destination"
+                    class="rm-border w-100"
+                />
             </IconField>
             <IconField class="col-none-11 col-md-3 p-0 m-1">
-                <DatePicker v-model="date" class="rm-border w-100" placeholder="Fecha" showIcon iconDisplay="input" />
+                <DatePicker
+                    v-model="date"
+                    :min-date="today"
+                    class="rm-border w-100"
+                    placeholder="Fecha"
+                    showIcon
+                    iconDisplay="input"
+                />
             </IconField>
 
             <IconField class="col-none-11 col-md-3 p-0 m-1">
-                <InputNumber class="rm-border w-100" v-model="passengers" inputId="minmax-buttons" showButtons
-                    placeholder="Pasajeros" :min="0" :max="4" fluid />
+                <InputNumber
+                    class="rm-border w-100"
+                    v-model="passengers"
+                    inputId="minmax-buttons"
+                    showButtons
+                    placeholder="Pasajeros"
+                    :min="0"
+                    :max="4"
+                    fluid
+                />
             </IconField>
         </div>
-        <Button label="Buscar" type="submit"
-            class="col-md-2 d-none d-md-block btn-trip-finder w-none-100 w-md-auto h-100 p-4" />
-        <Button label="Buscar" type="submit" class="col-none-2 d-block d-md-none btn-trip-finder-phone w-100 p-3" />
+        <Button
+            label="Buscar"
+            type="submit"
+            class="col-md-2 d-none d-md-block btn-trip-finder w-none-100 w-md-auto h-100 p-4"
+        />
+        <Button
+            label="Buscar"
+            type="submit"
+            class="col-none-2 d-block d-md-none btn-trip-finder-phone w-100 p-3"
+        />
     </form>
 </template>
 <script setup>
@@ -35,18 +72,18 @@ const origin = ref(null);
 const date = ref(null);
 const passengers = ref(null);
 const data = ref({});
-
+const today = ref(new Date());
 const submitFinder = () => {
     data.value = {
         origin: origin.value.getPlace(),
         destination: destination.value.getPlace(),
         date: date.value,
-        passengers: passengers.value
-    }
+        passengers: passengers.value,
+    };
 
     router.push({
-        name: 'TripsIndex',
-        query: { data: JSON.stringify(data.value) }
+        name: "TripsIndex",
+        query: { data: JSON.stringify(data.value) },
     });
 };
 
